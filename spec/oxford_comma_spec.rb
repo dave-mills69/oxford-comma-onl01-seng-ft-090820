e48d12911aebfb857488bc802645d55ea31d6129
David Mills <david_mills109@yahoo.com>
2020-09-15 23:31:07 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -1,12 +1,3 @@
-def ox
-
-
-
-
-
-
-
-
 describe "#oxford_comma" do
   it 'returns a string without any additional formatting when given a 1-element array' do
     expect(oxford_comma(["kiwi"])).to eq("kiwi")
