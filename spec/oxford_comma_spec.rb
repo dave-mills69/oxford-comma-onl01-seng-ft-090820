21d53302d8829506a5d82c48c5b6212d837438c7
David Mills <david_mills109@yahoo.com>
2020-09-15 23:30:00 +0000

    Automatically backed up by Learn

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -13,3 +13,20 @@ describe "#oxford_comma" do
     expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos")
   end
 end
+
+array = ["apple"]
+def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
+end
+
+oxford_comma(array)
+
+
